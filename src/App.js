b7b506ddbfc08deabea5363331a5c9d600f0ad00
guojin <jerrychane@126.com>
2020-06-20 10:23:01 +0800

    test the demo component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
-import TestDemo from 'prewarning';
-import 'prewarning/dist/main.css';
+import TestDemo  from 'prewarning';
+import  'prewarning/dist/main.css';
 function App() {
   return (
-    <div className="App">
+    <div>
        <TestDemo/>
     </div>
   );
