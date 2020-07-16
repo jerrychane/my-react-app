0d6191d6bfa0955bd4ce8aca0c36fe0625cd7f55
guojin <jerrychane@126.com>
2020-07-16 20:38:07 +0800

    1.0.5 默认时区显示、取消行内编辑和删除按钮的默认事件和冒泡

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('app')
+ReactDOM.render( < App / > ,
+    document.getElementById('app')
 );
