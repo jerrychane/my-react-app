fe1a873a73c43852ef2fd3340e848213801969bb
guojin <jerrychane@126.com>
2020-07-28 14:14:51 +0800

    修改产出目录和nginx配置

---
+++ dist/precache-manifest.0b6de354272479b07447edae32bc4976.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc649168f19a5818adf3000666125677",
+    "url": "./index.html"
+  },
+  {
+    "revision": "88118f17e90a8f40206d",
+    "url": "./static/css/2.9658521f.chunk.css"
+  },
+  {
+    "revision": "79426584801f7c893b85",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "88118f17e90a8f40206d",
+    "url": "./static/js/2.73874b83.chunk.js"
+  },
+  {
+    "revision": "2e660c03dbf175b104f1f2e87948fc06",
+    "url": "./static/js/2.73874b83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79426584801f7c893b85",
+    "url": "./static/js/main.ae1dda35.chunk.js"
+  },
+  {
+    "revision": "e21397ecf58f77891282",
+    "url": "./static/js/runtime-main.8ee73f49.js"
+  }
+]);
